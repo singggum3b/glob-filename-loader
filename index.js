a883ae6d5bb8f517f72906b8de4203bdf69b3c36
singgum3b <singgum3b@gmail.com>
2015-11-27 11:26:47 +0700

    - update script & package

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = function(content, sourceMap) {
 	this.debug && console.log(glob.sync(content.trim()));
 	var query =  loaderUtils.parseQuery(this.query);
 	return "module.exports = " + JSON.stringify(glob.sync(content.trim()).map(function (item,index) {
-				return query.publicPath + "/" + path.basename(item);
+				return query.publicPath + "/" + path.relative(query.root,item);
 			}));
 };
