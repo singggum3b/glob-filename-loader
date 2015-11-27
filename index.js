cf232a37dab38f76ed3ab226e739db21eeeb283a
singgum3b <singgum3b@gmail.com>
2015-11-27 10:55:51 +0700

    Initial push

---
+++ index.js
@@ -0,0 +1,11 @@
+var loaderUtils = require("loader-utils");
+var glob = require("glob");
+var path = require("path");
+
+module.exports = function(content, sourceMap) {
+	this.debug && console.log(glob.sync(content.trim()));
+	var query =  loaderUtils.parseQuery(this.query);
+	return "module.exports = " + JSON.stringify(glob.sync(content.trim()).map(function (item,index) {
+				return query.publicPath + "/" + path.basename(item);
+			}));
+};
